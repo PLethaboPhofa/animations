5225732040bf1657086b1e9937a8b27d8e2463e2
lethabo <your.email@example.com>
2024-03-03 00:32:47 +0200

    splines

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+//https://stackoverflow.com/questions/64678768/how-can-i-make-a-curve-line-to-join-two-node-in-html-and-css#:~:text=Afterwards:,that%20path%20to%20the%20svg
+
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +8,70 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  connections2:Record<string,HTMLElement>[] = [];
+
+  ngOnInit(): void {
+    this.collectElements();
+    this.connectElements();
+  }
+
+  collectElements() {
+    let holders = document.getElementsByClassName('col1');
+    let text = document.getElementsByClassName('text').item(0);
+
+    for (let count = 0; count < holders.length; count++) {
+      let holder = holders.item(count) as HTMLElement
+      this.connections2.push({
+        start: holder,
+        end: text as HTMLElement,
+      });
+    }
+  }
+
+  connectElements() {
+    let paths = document.getElementById('paths');
+    let padding = 10;
+    let bezierWeight = 0.5;
+    let oldPaths = paths?.children || [];
+    for (let a = oldPaths.length - 1; a >= 0; a--) {
+      paths?.removeChild(oldPaths[a]);
+    }
+
+    let x1, y1, x4, y4, dx, x2, x3, path, start, end;
+
+    for (let a = 0; a < this.connections2?.length; a++) {
+      start = this.connections2[a]?.['start'];
+      end = this.connections2[a]?.['end'];
+
+      x1 = start.offsetLeft + start.scrollWidth / 2 - padding;
+      y1 = start.offsetTop + start.clientHeight / 2 - padding;
+      x4 = end.offsetLeft + start.clientWidth / 2 - 450;
+      y4 = end.offsetTop + start.clientHeight/ 2 - padding +10;
+      dx = Math.abs(x4 - x1) * bezierWeight;
+
+      if (x4 < x1) {
+        x2 = x1 - dx;
+        x3 = x4 + dx;
+      } else {
+        x2 = x1 + dx;
+        x3 = x4 - dx;
+      }
+
+      let data = `M${x1+400} ${y1} C ${x2} ${y1} ${x3} ${y4} ${x4} ${y4}`;
+
+      path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      path.setAttribute('d', data);
+      path.setAttribute('class', 'path');
+      if(a!=this.connections2.length-1){
+        // path.setAttribute('style', 'fill: none;stroke: #000000;stroke-width: 6;');
+        paths?.appendChild(path);
+      }
+
+      
+    }
+  }
 }
